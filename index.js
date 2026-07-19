887fda6b2c3a577037c97e2702e3db6b93c26a33
Dusu Timothy <dususunday@gmail.com>
2026-07-19 10:40:13 +0100

    Add staff child class

--- index.js
+++ index.js
@@ -8,4 +8,27 @@ class SchoolManagementSystem {
     this.classAssigned = classAssigned;
   }
 
- 
+  // For child classes to verify the password
+  verifyPassword(inputPassword) {
+    return this.#password === inputPassword;
+  }
+
+  editDetails(id, newName, newPassword) {
+    if (id !== this.id) {
+      console.log("Invalid User-ID");
+      return;
+    }
+    console.log("Update Details");
+    this.fullName = newName ?? this.fullName;
+    this.#password = newPassword ?? this.#password;
+    console.log(`${id}'s details updated successfully`);
+  }
+}
+
+class Staffs extends SchoolManagementSystem {
+  constructor(fullName, id, password, classAssigned, grades) {
+    super(fullName, id, password, classAssigned);
+    this.grades = grades;
+  }
+}
+
